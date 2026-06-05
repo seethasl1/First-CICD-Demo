5348cf5049135ffa70b551840cfa8a2938cf9d9c
Your Name <your-email@example.com>
2026-06-05 09:43:27 +0530

    Add project files.

---
+++ First-CICD-Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace First_CICD_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
